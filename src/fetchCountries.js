caa4763ec47b5be4e74d99fb112531b9ef4df7d3
RomanZinoviev <rzinovijev@gmail.com>
2022-02-11 09:27:23 +0200

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,8 @@
-import Notiflix from 'notiflix';
-
-const countryInfoEl = document.querySelector(".country-info");
-
 export const fetchCountries = (name) => {
     return fetch(`https://restcountries.com/v3.1/name/${name}`).then(
         (response) => {
-            if (!response.ok) { 
-                countryInfoEl.innerHTML = "";
-                throw Notiflix.Notify.failure('Oops, there is no country with that name');                
+            if (!response.ok) {                 
+                throw new Error(response.status);                
             }
             return response.json();
         }
--- src/index.js
+++ src/index.js
@@ -9,35 +9,36 @@ const serchInputEl = document.querySelector("#search-box");
 
 const inputSerchHandle = (event) => { 
     const text = event.target.value.trim();            
-    fetchCountries(text).then(e => { doWaysOfMarkup(e); console.log(e)}).catch(err => { console.log(err) });    
+    fetchCountries(text).then(e => { doWaysOfMarkup(e); console.log(e) }).catch(err => { Notiflix.Notify.failure('Oops, there is no country with that name'); countryInfoEl.innerHTML = ""; });    
 };
 const doMarkupShort = (countryes) => {
     const markup = countryes.map(({ name, flags }) => {
         return `<li class="contry">
-                <h2>${name.official}</h2>
-                <img src="${flags.svg}" alt="" class="contry__flag" width=40px>                
+                <img src="${flags.svg}" alt="" class="contry__flag" width=60px>
+                <h2 class="country__name">${name.official}</h2>
+                                
         </li>`
     }).join("");
     countryInfoEl.innerHTML = markup;
 };
 const doMarkupFull = (country) => { 
     const markup = country.map(({ name, capital, population, flags, languages }) => {
-        return `<li class="contry">
+        return `<li class="contry__full">
                 <h2>${name.official}</h2>
                 <img src="${flags.svg}" alt="" class="contry__flag" width=120px>
-                <p class="contry__capital">Capital: ${capital}</p>
-                <p class="contry__population">Population: ${population}person</p>
-                <p class="contry__languages">Languages: ${Object.values(languages)}</p>    
+                <p class="contry__capital"><span class="coutry__text">Capital:</span> ${capital}</p>
+                <p class="contry__population"><span class="coutry__text">Population:</span> ${population} person</p>
+                <p class="contry__languages"><span class="coutry__text">Languages:</span> ${Object.values(languages)}</p>    
         </li>`
     }).join("");
     countryInfoEl.innerHTML = markup;
 };
 const doWaysOfMarkup = (array) => {
-    if (array.length > 10) { return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.'); }
+    if (array.length > 10) { countryInfoEl.innerHTML = ""; return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.'); }
     if (array.length >= 2 && array.length <= 10) { return doMarkupShort(array) }
     if (array.length === 1) { return doMarkupFull(array) }
     else { return countryInfoEl.innerHTML = ""; }
 };
 
-serchInputEl.addEventListener("input", debounce(inputSerchHandle, DEBOUNCE_DELAY))
+serchInputEl.addEventListener("input", debounce(inputSerchHandle, DEBOUNCE_DELAY));
 
